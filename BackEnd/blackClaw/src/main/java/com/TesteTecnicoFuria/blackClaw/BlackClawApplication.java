4040065d5d85946dcbc173f896045e6fa1a03e61
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2025-04-30 01:38:30 -0300

    BackEnd Initial commit

---
+++ BackEnd/blackClaw/src/main/java/com/TesteTecnicoFuria/blackClaw/BlackClawApplication.java
@@ -0,0 +1,13 @@
+package com.TesteTecnicoFuria.blackClaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackClawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackClawApplication.class, args);
+	}
+
+}
---
+++ BackEnd/blackClaw/src/test/java/com/TesteTecnicoFuria/blackClaw/BlackClawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TesteTecnicoFuria.blackClaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackClawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
